04115b40a37586419f6181e32111c8ec7803a160
choeungeol <ceg0416@naver.com>
2017-01-12 22:21:16 +0900

    2017-01-12

--- database/migrations/2017_01_02_032851_add_pinfo_tables.php
+++ database/migrations/2017_01_02_032851_add_pinfo_tables.php
@@ -12,12 +12,12 @@ class AddPinfoTables extends Migration
      */
     public function up()
     {
-        Schema::table('person_infos', function(Blueprint $table)
+/*        Schema::table('person_infos', function(Blueprint $table)
         {
             $table->integer('company_basicinfo_id')->unsigned()->nullable();
             $table->foreign('company_basicinfo_id')->references('id')->on('company_basicinfo');       //부서 ID
 
-        });
+        });*/
     }
 
     /**
@@ -27,12 +27,12 @@ class AddPinfoTables extends Migration
      */
     public function down()
     {
-        Schema::table('person_infos', function(Blueprint $table)
+/*        Schema::table('person_infos', function(Blueprint $table)
         {
             // delete above columns
             $table->dropForeign('person_infos_company_basicinfo_id_foreign');
             $table->dropColumn('company_basicinfo_id');
         });
-    }
+    }*/
 
 }
